f60ab24ccc4ea3f2554bdc46192d5e9393c3765e
BrandonY <122407502+by36@users.noreply.github.com>
2023-03-07 14:05:01 -1000

    Update index.js
    Query by city

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 //displays index.html of root path
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
 //invoked after hitting go in the html form
+<<<<<<< HEAD
+app.post("/", function (req, res) {
+
+   // takes in the CityID from the html form, display in console. Takes in as string
+  const cityId = req.body.cityId;
+
+      //build up the URL for the JSON query, API Key is secret and needs to be obtained by signup 
+  const units = "imperial";
+  const apiKey = "839ff57e9450018e3010817d6f50adef";
+  const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=${units}`;
+
+      // this gets the data from Open Weather API
+  https.get(url, function (response) {
+=======
 app.post("/", function(req, res) {
     
     // takes in the latitude and longitude from the html form, display in console. Takes in as string
@@ -50,9 +65,39 @@ app.post("/", function(req, res) {
         });
     });
 })
+>>>>>>> origin/master
 
+    // gets individual items from Open Weather API
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const cityName = weatherData.name;
+      const temperature = weatherData.main.temp;
+      const humidity = weatherData.main.humidity;
+      const windSpeed = weatherData.wind.speed;
+      const windDirection = weatherData.wind.deg;
+      const cloudiness = weatherData.clouds.all;
 
+      // displays the output of the results
+      const responseText = `
+        <h1>The city is ${cityName}</h1>
+        <p>The current Temperature is: ${temperature} &#8457;</p>
+        <p>The current Humidity is: ${humidity}%</p>
+        <p>The current Wind Speed is: ${windSpeed} mph</p> 
+        <p>The current Wind Direction is: ${windDirection} &#176;</p>
+        <p>The current level of Cloudiness is: ${cloudiness}%</p>
+      `;
+
+      res.send(responseText);
+    });
+  });
+});
+
+<<<<<<< HEAD
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
+=======
 //Code will run on 3000 or any available open port
 app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on port");
+>>>>>>> origin/master
 });
